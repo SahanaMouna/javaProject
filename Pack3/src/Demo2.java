e028697513bb83b1a34d475421fd4f760119fdc7
SahanaMouna <mounagowda150@gmail.com>
2024-09-25 11:49:55 +0530

    Add files via upload

---
+++ Pack3/src/Demo2.java
@@ -0,0 +1,11 @@
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		int number_1=30;
+		System.out.println("hello");
+
+	}
+
+}
---
+++ Pack3/src/pack4/Demo4.java
@@ -0,0 +1,18 @@
+package pack4;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		int number_1=30;
+		float number_2=40;
+		char a;
+		double number_3;
+		System.out.println("hello");
+		System.out.println(number_1);
+		System.out.println("'a','b'");
+		System.out.println("6");
+		System.out.println("34.568934543532");
+		
+	}
+
+}
